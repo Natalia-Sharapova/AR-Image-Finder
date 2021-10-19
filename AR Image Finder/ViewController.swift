8acda091420aec00effc5a5ff7d66b75013946b1
Natalia Sharapova <emuradyants2005@gmail.com>
2021-10-19 20:29:09 +0300

    Added fading animation

--- AR Image Finder/ViewController.swift
+++ AR Image Finder/ViewController.swift
@@ -5,13 +5,18 @@
 //  Created by Наталья Шарапова on 17.10.2021.
 //
 
-import UIKit
-import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    let videoPlayer: AVPlayer = {
+        let url = Bundle.main.url(forResource: "mountains",
+                                  withExtension: "mp4",
+                                  subdirectory: "art.scnassets")!
+        return AVPlayer(url: url)
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +26,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-    
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,8 +41,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)!
         
         //Detect planes
-       // configuration.planeDetection = [.horizontal]
-
+        // configuration.planeDetection = [.horizontal]
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -48,7 +53,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
@@ -62,36 +67,49 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             print("Found unknown anchor")
         }
     }
+    
+    func nodeAdded(_ node: SCNNode, for imageAnchor: ARImageAnchor) {
+        //Get image size
+        let image = imageAnchor.referenceImage
+        let size = image.physicalSize
         
-        func nodeAdded(_ node: SCNNode, for imageAnchor: ARImageAnchor) {
-            //Get image size
-            let image = imageAnchor.referenceImage
-            let size = image.physicalSize
-            
-            //Create plane of the same size
-            let height = 66 / 65 * size.height
-            let width = image.name == "horses" ?
-                156 / 150 * 15 / 6.269 * size.width:
-               156 / 150 * 15 / 6.1345 * size.width
-            
-            let plane = SCNPlane(width: width, height: height)
-            plane.firstMaterial?.diffuse.contents = image.name == "horses" ?
-                UIImage(named: "100dollarsface"): UIImage(named: "100dollarshouse")
+        //Create plane of the same size
+        let height = 66 / 65 * size.height
+        let width = image.name == "horses" ?
+            156 / 150 * 15 / 6.269 * size.width:
+            156 / 150 * 15 / 6.1345 * size.width
         
-            //Create node
-            let planeNode = SCNNode(geometry: plane)
-            planeNode.eulerAngles.x = -.pi / 2
+        let plane = SCNPlane(width: width, height: height)
+        plane.firstMaterial?.diffuse.contents = image.name == "horses" ?
+            UIImage(named: "100dollarsface"):
+            videoPlayer
+        
+        if image.name == "theatre" {
+            videoPlayer.play()
+        }
+        
+        //Create node
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        //Move planeNode
+        planeNode.position.x -= image.name == "horses" ? 0.01 : 0
+        planeNode.position.x += image.name == "theatre" ? 0.01 : 0
+        
+        //Run animation
+        planeNode.runAction(.sequence([
+            .wait(duration: 5),
+            .fadeOut(duration: 3),
+            .removeFromParentNode(),
             
-            //Move planeNode
-           planeNode.position.x -= image.name == "horses" ? 0.01 : 0
-            planeNode.position.x += image.name == "theatre" ? 0.01 : 0
-            
-            //Add planeNode to the given node
-            node.addChildNode(planeNode)
+        ]))
+        
+        //Add planeNode to the given node
+        node.addChildNode(planeNode)
     }
-        
-        func nodeAdded(_ node: SCNNode, for planeAnchor: ARPlaneAnchor) {
-            print("Plane \(planeAnchor) added")
+    
+    func nodeAdded(_ node: SCNNode, for planeAnchor: ARPlaneAnchor) {
+        print("Plane \(planeAnchor) added")
     }
 }
 
